61b1c0d906dddcfd909c8c5a1dbc9bbb4a6a827f
Arthur Vinicius Alves <avas@ic.ufal.br>
2025-04-28 16:11:42 -0300

    huffman em portugues

---
+++ huffman_pt/huffman.h
@@ -0,0 +1,20 @@
+#ifndef HUFFMAN_H
+#define HUFFMAN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+typedef struct No {
+    unsigned char caractere;
+    int frequencia;
+    struct No *esquerda;
+    struct No *direita;
+    struct No *proximo;
+} No;
+
+void comprimir(const char *nome_entrada, const char *nome_saida);
+void descomprimir(const char *nome_entrada, const char *nome_saida);
+void liberar_arvore(No *raiz);
+
+#endif // HUFFMAN_H
